88a601ca0bd0ec5f7778bf09fe50e27ef363d749
oshanedesign <oshane.design@gmail.com>
2022-05-31 17:38:45 -0500

    create query

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO "public"."animals"("id","name","date_of_birth","escape_attempts","neutered","weight_kg")
+VALUES
+(1,E'Agumon',E'2020-02-03',0,TRUE,10.23),
+(2,E'Gabumon',E'2018-11-15',2,TRUE,8),
+(3,E'Pikachu',E'2021-01-07',1,FALSE,15.04),
+(4,E'Devimon',E'2017-05-12',5,TRUE,11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT name FROM animals WHERE '[2016-01-01, 2019-12-31]'::daterange @> date_of_birth;
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name in ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3; 
